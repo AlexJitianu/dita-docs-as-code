17e432beabddc647281669c104d9f9a9642357d1
Alex Jitianu <alex_jitianu@sync.ro>
2021-10-21 09:05:42 +0300

    Update publish.sh
    Use Oxygen Publishing Engine.

--- scripts/publishing/publish.sh
+++ scripts/publishing/publish.sh
@@ -1,32 +1,17 @@
-DITA_OT=bin/dita-ot-3.3.3
+DITA_OT=bin/oxygen-publishing-engine-3.x
 
 echo "Downloading DITA-OT"
-if [[ ! -e bin/dita-ot.zip ]];
+if [[ ! -e bin/oxygen-publishing-engine-3.x.zip ]];
 then
   mkdir bin
   
-  curl -q -L https://github.com/dita-ot/dita-ot/releases/download/3.3.3/dita-ot-3.3.3.zip --output bin/dita-ot.zip
+  curl -q -L https://www.oxygenxml.com/InstData/PublishingEngine/oxygen-publishing-engine-3.x.zip --output bin/oxygen-publishing-engine-3.x.zip
   
-  ls -l bin/dita-ot.zip
+  ls -l bin/oxygen-publishing-engine-3.x.zip
   
-  unzip -q bin/dita-ot.zip -d bin/
+  unzip -q bin/oxygen-publishing-engine-3.x.zip -d bin/
 fi
 
-echo "====================================="
-echo "download WebHelp plugin"
-echo "====================================="
-
-curl -q -L https://mirror.oxygenxml.com/InstData/WebHelp/oxygen-webhelp-dot-3.x.zip  --output bin/oxygen-webhelp-dot-2.x.zip
-
-
-echo "====================================="
-echo "extract WebHelp to DITA-OT"
-echo "====================================="
-unzip -q bin/oxygen-webhelp-dot-2.x.zip -d bin/
-
-cp -R bin/com.oxygenxml.* $DITA_OT/plugins/
-
-pwd
 
 cp scripts/publishing/licensekey.txt $DITA_OT/plugins/com.oxygenxml.webhelp.responsive/licensekey.txt
 
